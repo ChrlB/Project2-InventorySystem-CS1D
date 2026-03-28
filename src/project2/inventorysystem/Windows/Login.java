00014d76b39424243189ac8fe59b06b3bbcfecec
ChrlB <johnbonaobra106@gmail.com>
2026-03-28 22:26:27 +0800

    prepare window files

---
+++ src/project2/inventorysystem/Windows/Login.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project2.inventorysystem;
+
+/**
+ *
+ * @author user
+ */
+import javax.swing.JFrame;
+
+public class Login extends JFrame {
+  
+  Login(){
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(null);
+    this.setSize(700,500);
+    this.setVisible(true);
+  }
+}
