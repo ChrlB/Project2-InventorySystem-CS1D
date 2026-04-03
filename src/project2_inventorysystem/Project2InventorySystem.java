90721c96d84e045fd485b05e0467d011640b820c
ChrlB <johnbonaobra106@gmail.com>
2026-04-03 21:06:20 +0800

    connect to database

--- src/project2_inventorysystem/Project2InventorySystem.java
+++ src/project2_inventorysystem/Project2InventorySystem.java
@@ -3,7 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package project2_inventorysystem;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import project2_inventorysystem.Windows.*;
+import java.sql.*;
 /**
  *
  * @author user
@@ -14,7 +18,29 @@ public class Project2InventorySystem {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    new Login();
+    
+    try{
+      Properties prop = new Properties();
+      prop.load(new FileInputStream("db.properties"));
+
+      String url = prop.getProperty("DB_URL");
+      String user = prop.getProperty("DB_USER");
+      String pass = prop.getProperty("DB_PASS");
+      
+      Connection conn = DriverManager.getConnection(
+        url,
+        user,
+        pass
+      );
+      //new Login();
+      System.out.print(conn);
+      
+      
+      
+      
+    }catch(Exception ex){
+      System.out.print(ex);
+    }
     //new Dashboard();
   }
   
