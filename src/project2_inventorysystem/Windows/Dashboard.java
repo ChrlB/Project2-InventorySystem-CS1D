e6c6e5df419ac8e608ddd85ad3224c359756ea8a
AQUA24-rgb <juanvictorvillanueva66@gmail.com>
2026-04-15 19:52:21 +0800

    add header on user window

--- src/project2_inventorysystem/Windows/Dashboard.java
+++ src/project2_inventorysystem/Windows/Dashboard.java
@@ -62,7 +62,7 @@ public class Dashboard extends JFrame{
       order_btn.addActionListener(e -> {  new Order(user_ID, conn); this.dispose();  });
       product_btn.addActionListener(e -> {new Product(conn);this.dispose();});
 //      sales_btn.addActionListener(e -> new Sales());
-//      user_btn.addActionListener(e -> new User());
+      user_btn.addActionListener(e ->{new User(conn); this.dispose();});
       
       order_icon = new IconBuilder("/project2_inventorysystem/Windows/Icons/menu.png",30,40,100,90);
       product_icon = new IconBuilder("/project2_inventorysystem/Windows/Icons/product.png",30,140,100,90);
--- src/project2_inventorysystem/Windows/User.java
+++ src/project2_inventorysystem/Windows/User.java
@@ -4,13 +4,38 @@
  */
 package project2_inventorysystem.Windows;
 
+import java.awt.Color;
+import javax.swing.JFrame;
 import org.mindrot.jbcrypt.BCrypt;
+import java.sql.*;
+import project2_inventorysystem.Windows.MyComponents.*;
 
 /**
  *
  * @author user
  */
-public class User {
+public class User extends JFrame{
+    Header header;
+    User(Connection conn){
+        header = new Header();
+        
+        
+        
+        
+        
+        
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("User");
+        this.setLayout(null);
+        this.setResizable(false);
+        this.setSize(1270,650);
+        this.getContentPane().setBackground(new Color(0xD3C3B9));
+        this.add(header);
+        this.setVisible(true);
+    }
+    
+    
 //  String hashed = BCrypt.hashpw("admin123", BCrypt.gensalt(12));
 //      String sql = "insert into users(username,password,fullname) values(?,?,?)";
 //      pstmt = conn.prepareStatement(sql);
