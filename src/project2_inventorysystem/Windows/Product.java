1a27dc2a17dba227ee50c075385de4ab246c7086
RiceShower-0305 <paladosyrilljohn@gmail.com>
2026-04-16 18:55:09 +0800

    add new buttons in product

--- src/project2_inventorysystem/Windows/Product.java
+++ src/project2_inventorysystem/Windows/Product.java
@@ -22,6 +22,7 @@ public class Product extends JFrame{
                 delete_btn,
                 update_btn,
                 restock_btn;
+  TextFieldBuilder product_id_field, product_name_field, category_name_field, unit_price_field, stock_quantity_field;
     Product(int userID, Connection conn){
       this.conn = conn;
       user_ID = userID;
@@ -32,11 +33,23 @@ public class Product extends JFrame{
       update_btn = new ButtonBuilder("UPDATE", 50, 170, 200, 50, 15);
       restock_btn = new ButtonBuilder("RESTOCK", 50, 230, 200, 50, 15);
       
+      product_id_field = new TextFieldBuilder(false, 250, 50, 200, 50, 15); 
+      product_name_field = new TextFieldBuilder(true, 250, 110, 200, 50, 15);
+      category_name_field = new TextFieldBuilder(true, 250, 170, 200, 50, 15);
+      unit_price_field = new TextFieldBuilder(true, 250, 230, 200, 50, 15);
+      stock_quantity_field = new TextFieldBuilder(true, 250, 290, 200, 50, 15);
+      
       product_form_panel = new JPanel();
       product_form_panel.setBounds(0, 100, 500, 550);
       product_form_panel.setBackground (new Color(0XB58863));
       product_form_panel.setLayout(null);
       
+      product_form_panel.add(product_id_field);
+      product_form_panel.add(product_name_field);
+      product_form_panel.add(category_name_field);
+      product_form_panel.add(unit_price_field);
+      product_form_panel.add(stock_quantity_field);
+    
       product_form_panel.add(add_btn);
       product_form_panel.add(delete_btn);
       product_form_panel.add(update_btn);
