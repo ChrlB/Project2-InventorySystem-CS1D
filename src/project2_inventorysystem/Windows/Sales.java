1e35dda96589a0dff24b1e8c64d36c08b12215d9
AQUA24-rgb <juanvictorvillanueva66@gmail.com>
2026-04-23 19:16:46 +0800

    add table on sales

--- src/project2_inventorysystem/Windows/Sales.java
+++ src/project2_inventorysystem/Windows/Sales.java
@@ -5,9 +5,15 @@
 package project2_inventorysystem.Windows;
 
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import project2_inventorysystem.Windows.MyComponents.Header;
+import project2_inventorysystem.Windows.MyComponents.TableBuilder;
 
 /**
  *
@@ -18,11 +24,36 @@ public class Sales extends JFrame{
     Connection conn;
     Header header;
     
+    TableBuilder sales_tbl;
+    JScrollPane sales_tbl_scrollpane;
+    
+    ResultSet rs;
+    String sql;
+    PreparedStatement pstmt;
+    
    public Sales(int userID,Connection conn){
       try{            
         this.conn = conn;
         user_ID = userID;
         header = new Header();
+        
+        sql = """
+              SELECT *
+              FROM tbl_sales
+              """;
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+        
+        sales_tbl = new TableBuilder(rs);
+        sales_tbl.addMouseListener(new MouseAdapter() {
+         @Override
+          public void mouseReleased(MouseEvent e) {  
+              //showSelectedRecord();
+          }
+        });
+        
+        sales_tbl_scrollpane = new JScrollPane(sales_tbl);
+        sales_tbl_scrollpane.setBounds(500,150,725,350);
           
          this.addWindowListener(new java.awt.event.WindowAdapter() {
           @Override
@@ -42,7 +73,9 @@ public class Sales extends JFrame{
         
         
         this.add (header);
+        this.add(sales_tbl_scrollpane);
         this.setVisible(true);
+        
       }catch(Exception ex){
         System.out.println(ex);
       }
