361c1ff2aec3f2dc79a840f89b24312ae3b99826
RiceShower-0305 <paladosyrilljohn@gmail.com>
2026-04-29 18:35:46 +0800

    added header in UserLogs

--- src/project2_inventorysystem/Windows/UserLogs.java
+++ src/project2_inventorysystem/Windows/UserLogs.java
@@ -4,10 +4,41 @@
  */
 package project2_inventorysystem.Windows;
 
+import java.awt.Color;
+import java.sql.Connection;
+import javax.swing.*;
+import project2_inventorysystem.Windows.MyComponents.Header;
+
 /**
  *
  * @author user
  */
-public class UserLogs {
-  
+public class UserLogs extends JFrame {
+      Header header;
+
+    UserLogs(int userID,Connection conn){
+        try{
+                header = new Header();
+            this.addWindowListener(new java.awt.event.WindowAdapter() {
+              @Override
+              public void windowClosing(java.awt.event.WindowEvent e) {
+              new User(userID, conn);
+                dispose();
+              }
+            });
+
+            this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            this.setTitle("USER");
+            this.setLayout(null);
+            this.setResizable(false);
+            this.setSize(1270,580);
+            this.getContentPane().setBackground(new Color(0xD3C3B9));
+            this.setLocationRelativeTo(null);
+            this.add(header);
+            this.setVisible(true);
+        }catch(Exception ex){
+        System.out.println(ex);
+      }
+        
+  }
 }
